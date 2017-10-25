964ca682e326a21138a22031dad7b9ec6ca1c9af
spences10 <spences10apps@gmail.com>
2017-10-25 18:53:56 +0100

    style the counter

--- src/components/Label.js
+++ src/components/Label.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import styled from 'styled-components'
 
-class Label extends Component {
-  render() {
-    return (
-      <label>0ms</label> 
-    )
-  }
+const Label = styled.label`
+  border-radius: 3px;
+  padding: 0.25em 1em;
+  margin: 0 1em;
+  background: transparent;
+  color: palevioletred;
+  border: 2px solid palevioletred;
+`
+class Counter extends Component {
+render() {
+  return (
+    <Label>0ms</Label>
+  )
 }
-
-export default Label
+}
+export default Counter
