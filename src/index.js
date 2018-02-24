4e8d459fb1ad73b4bbc03c332267e2738929f140
spences10 <spences10apps@gmail.com>
2018-02-24 18:13:30 +0000

    remove css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+import { ThemeProvider } from 'styled-components'
+
 import App from './App'
+import { theme } from './theme/globalStyle'
+
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+
+  document.getElementById('root')
+)
 registerServiceWorker()
